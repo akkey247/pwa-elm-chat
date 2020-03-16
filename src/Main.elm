53492e2addb6d5b17f10b5731cba5ad83ab0575e
akkey <akkey52417@gmail.com>
2020-03-16 23:28:33 +0900

    とりあえずBrowser.elementを使った形に修正

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,66 @@
-module Main exposing(main)
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
 
 
-import Html exposing (text)
+
+-- MAIN
 
 
 main =
-  text "Hello World!!"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( Model "Hello World!!"
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update _ model =
+    ( model
+    , Cmd.none
+    )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text model.name ] ]
